93d3611e0a9a5f6604e3280f396fe4ffe092687a
Haftu Reda <haftureda@gmail.com>
2019-07-09 21:24:43 -0600

    "Update"

--- jsanim.js
+++ jsanim.js
@@ -4,3 +4,10 @@ document.getElementById("start").onclick=function(){
 document.getElementById("stop").onclick=function(){
     
 }
+
+function animationText() {
+    var textAreaObj = document.getElementById("textArea");
+    var choice = document.getElementById("anitype").value;   
+    textAreaObj.value = ANIMATIONS[choice];
+    textArray = (textAreaObj.value).split("=====\n");
+}
